7c017f1c59246c79cf692a14bbfd5ffcd47e3d1c
new_user <new_user@gmail.com>
2021-06-18 16:11:10 +0300

    Добавьте файлы проекта.

---
+++ EnumeratorSelect/LinqEnumerable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorSelect
+{
+    class Config
+    {
+        public bool isRemove { private set; get; }
+        public void Remove() => isRemove = true;
+    }
+    static class LinqEnumerable
+    {
+        public static IEnumerable<R> SelectAt<T,R>(this IEnumerable<T> enumerable,
+            Func<T,Config,R> func)
+        {
+            foreach (var item in enumerable)
+            {
+                Config config = new Config();
+                var result = func(item, config);
+                if (!config.isRemove) yield return result;
+            }
+        }
+    }
+}
---
+++ EnumeratorSelect/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumeratorSelect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Enumerable.Range(0, 100).SelectAt((a,q) =>
+            {
+                if (a % 2 == 0) q.Remove();
+                return a;
+            });
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
